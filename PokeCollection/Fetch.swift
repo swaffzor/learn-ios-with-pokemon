e1edd275f5037f2ba682193aa890c0b4ea73aab8
Jeremy Swafford <jeremy@swafford.co>
2021-08-06 05:56:49 -0400

    add service functions for fetching

--- PokeCollection/Fetch.swift
+++ PokeCollection/Fetch.swift
@@ -2,7 +2,49 @@
 //  Fetch.swift
 //  PokeCollection
 //
-//  Created by Jeremy Swafford on 8/5/21.
+//  Created by Jeremy Swafford on 6/24/21.
 //
 
 import Foundation
+
+func fetchPaginatedData(url: URL, completionHandler: @escaping (PokemonPaginated) ->  Void) {
+    
+    var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+//    components?.queryItems = [
+//        URLQueryItem(name: "limit", value: "60"),
+//        URLQueryItem(name: "offset", value: "60")
+//    ]
+    guard let resultURL = components?.url?.absoluteURL else {return}
+    
+    let task = URLSession.shared.dataTask(with: resultURL) { data, response, error in
+        if data != nil, error == nil {
+            if let responseText = String.init(data: data!, encoding: .ascii) {
+                let jsonData = responseText.data(using: .utf8)!
+                let temp = try! JSONDecoder().decode(PokemonPaginated.self, from: jsonData)
+                completionHandler(temp)
+            }
+        }
+    }
+    task.resume()
+}
+
+func fetchPokemonCells(url: String, completionHandler: @escaping (Pokemon) -> Void) {
+    guard let resultURL = URL(string: url) else {return}
+    URLSession.shared.dataTask(with: resultURL) { (data, response, err) in
+        guard let pokeData = data, err == nil else {return}
+        do {
+            let pokemonData = try JSONDecoder().decode(PokemonDTO.self, from: pokeData)
+            let myPokemon = Pokemon(from: pokemonData)
+            completionHandler(myPokemon)
+        } catch {
+            print("oh no \(error)")
+        }
+    }.resume()
+}
+
+func fetchImage(url: URL, completionHandler: @escaping (Data) -> Void) {
+    URLSession.shared.dataTask(with: url) {data, response, error in
+        guard let d = data, error == nil else {return}
+        completionHandler(d)
+    }.resume()
+}
